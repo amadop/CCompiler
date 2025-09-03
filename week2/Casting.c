61c40450102135be326c8aed656b48545906316b
akulacsathish-eng <akulac.sathish@gmail.com>
2025-09-03 15:42:15 +0000

    week2

--- week2/Casting.c
+++ week2/Casting.c
@@ -2,6 +2,20 @@
 
 int main()
 {
-    printf("Hello");
-    return 0;
+
+   int var1 ,var2; 
+
+   printf("Enter the first number");
+   scanf("%d",&var1);
+
+
+   printf("Enter the second number");
+    scanf("%d",&var2);
+    
+    printf("lt=%d", var1<var2);
+    printf("let=%d", var1<=var2);
+    printf("gt=%d", var1>var2);
+    printf("get=%d", var1>=var2);
+    printf("eq=%d", var1==var2);
+    printf("neq=%d", var1!=var2);
 }
