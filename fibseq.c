b02c66ab4da344260f2931db0b69385c454144ba
amadop <aplascencia3432@floridapoly.edu>
2025-10-15 16:44:56 +0000

    fibseq+recurs

--- fibseq.c
+++ fibseq.c
@@ -19,7 +19,15 @@ int fib_iterative(int n)
 }
 
 
-int fib_recursive(int n);
+int fib_recursive(int n)
+{
+     if(n == 0)
+        return 0;
+     if(n == 1)
+        return 1;
+    return fib_recursive(n-1)+fib_recursive(n-2);
+}
+
 
 
 int main() {
