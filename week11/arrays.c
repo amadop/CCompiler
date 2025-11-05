54b6b21e076abca58da9da60d62ce975bbf5ebd7
amadop <aplascencia3432@floridapoly.edu>
2025-11-05 17:24:18 +0000

    save2 arrays

--- week11/arrays.c
+++ week11/arrays.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
+void add_two1Darray(int size1, int p[size1],int size2,int q[size2])
+{
+    int r[size1];
+    for(int i=0;i<size1;i++)
+    {
+        r[i]=p[i]+q[i];
+        printf("%d ", r[i]);
 
+    }
+}
+
+void diff_2Darray(int rows, int cols, int p[rows][cols], int q[rows][cols])
+{
+    int r[rows][cols];
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            r[i][j]= p[i][j]- q[i][j];
+            printf("%d ", r[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main()
 {
     int arr[10],arr2[10],arr3[3][3],arr4[3][3];
@@ -22,17 +45,18 @@ int main()
             odd= odd+2;
         }
     }
-    printf("1D arrays are: \n");
+    printf("1D arrays are:\n");
         printf("1st array:\n");
         for(int i=0; i<10;i++)
         {
-            printf("%d ",arr[i]);
+            printf("%d ", arr[i]);
+            printf("\n");
         }
           printf("2nd array:\n");
         for(int i=0; i<10;i++)
         {
-            printf("%d ",arr2[i]);
-            
+            printf("%d ", arr2[i]);
+            printf("\n");
         }
     printf("2D arrays are: \n", arr3, arr4);
       printf("1st 2D array:\n");
